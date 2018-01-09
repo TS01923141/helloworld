da2ef68004aabef8f3e76687ff9aa04110403442
TYC <TS01923141@gmail.com>
2018-01-09 16:02:23 +0800

    button Event

--- src/Test.js
+++ src/Test.js
@@ -6,11 +6,6 @@ import {Home} from './components/Home';
 
 class Test extends Component{
   render(){
-      //
-      var user={
-          name: "Anna",
-          hobbies: ["Sports", "Reading"]
-      }
       return(
           <div className="container">
             <div className="row">
@@ -20,9 +15,7 @@ class Test extends Component{
             </div>
             <div className="row">
               <div className="col-xs-10 col-xs-offset-1">
-                <Home name={"Max"} age={27} user={user} >
-                    <p>This is a paragraph!</p>
-                </Home>
+                <Home name={"Max"} initialAge={27}/>
               </div>
             </div>
           </div>
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,28 +2,33 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 export class Home extends React.Component {
+    //ES6 初始化時載入
+    constructor(props){
+        super();
+        this.state = { 
+            age : props.initialAge,
+            status : 0
+        };
+    }
+    //使用時若使用onMakeOlder()是loading結束馬上執行 ex.onMakeOlder()
+    //     若使用onMakeOlder則為觸發時才執行 ex.onClick={this.onMakeOlder}
+    onMakeOlder(){
+        this.setState({
+            //修改自己class的資料
+            age: this.state.age + 3
+        });
+    }
+
     render(){
-        var text = "Something!";
         console.log(this.props);
         return(
             <div>
                 <p>In a new Component!</p>
-                /*印出Home的變數 */
-                <p>{text}</p>
-                /*印出Home在props的變數 */
-                <p>Your name is {this.props.name}, your age is {this.props.age}</p>
-                /*印出Home所在的props的變數user的name變數 */
-                <p>User Object => Name: {this.props.user.name}</p>
-                <div>
-                    <h4>Hobbies</h4>
-                    /* 迴圈用法，印出hobbies，命名為hobby，key為i(第i個 從0開始) */
-                    <ul>
-                        {this.props.user.hobbies.map((hobby,i) => <li key={i}>{hobby}</li>)}
-                    </ul>
-                </div>
+                <p>Your name is {this.props.name}, your age is {this.state.age}</p>
+                <p>Status: {this.state.status}</p>
                 <hr/>
-                /* 從home取出children */
-                {this.props.children}
+                {/* <button onClick={this.onMakeOlder.bind(this)} className="btn btn-primary">Make me older!</button> */}
+                <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
             </div>
         );
     }
@@ -31,7 +36,5 @@ export class Home extends React.Component {
 //告訴props該js(Home)需要有哪些propType
 Home.propTypes = {
     name : PropTypes.string,
-    age : PropTypes.number,
-    user : PropTypes.object,
-    children : PropTypes.element.isRequired
+    initialAge : PropTypes.number
 };
